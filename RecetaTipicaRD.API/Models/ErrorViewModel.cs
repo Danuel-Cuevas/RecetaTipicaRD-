1b85974d69010beb7936cbdebdc7dc3788919637
Danuel Ezequiel Cuevas Tejeda <dc.cosmelectric@gmail.com>
2025-08-16 04:02:25 -0400

    Proyect Template Created

---
+++ RecetaTipicaRD.API/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RecetaTipicaRD.API.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
